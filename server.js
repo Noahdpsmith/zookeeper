815681dd9de7b4712605278984e81f52d47fced5
noah smith <noahjosephsmith@gmail.com>
2022-01-09 15:54:58 -0700

    fixed heroku and js

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
+//dont touch
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
+//dont touch
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -42,6 +44,10 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal =>animal.id === id)[0];
+    return result;
+  }
   app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -49,6 +55,15 @@ function filterByQuery(query, animalsArray) {
     }
     res.json(results);
   });
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+  
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
